6905dfcd308b61d253aec1f96af963e3e4aa9bfe
Rotem Weintal <rotem.weintal@post.runi.ac.il>
2024-11-16 22:57:54 +0200

    add my first draft to git

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,18 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String cheer = args [0].toUpperCase();
+            int number = Integer.parseInt(args[1]);
+            String specialLetters = "AEFHILMNORSX";
+            int isSpecial =0; 
+            for (int i =0; i < cheer.length(); i++){
+                isSpecial = (specialLetters.indexOf(cheer.charAt(i)));
+                System.out.println("Give me " + (isSpecial == -1 ? "a" : "an") + " " + cheer.charAt(i) + ": " + cheer.charAt(i) + "!");
+            }
+            System.out.println("What does that spell?");
+            for (int j=0; j < number; j++){
+                System.out.println(cheer + "!!!");
+            }
         }
+        
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,34 @@
 // Demonstrates the Collatz conjecture.
+
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int valueN = Integer.parseInt(args[0]); 
+		String mode = args[1];
+		for (int seed = 1; seed <= valueN; seed++){
+			int number = seed;
+			int count =1;
+			String sequence = number + " ";
+			if (number == 1){ //Handle specific case where the seed is 1
+				 sequence += "4 2 1 ";
+				 count = 4;
+			}
+			while (number !=1){
+				if (number % 2 ==0){
+					number = number / 2;
+				}
+				else{
+					number = number * 3 + 1;
+				}
+				count ++;
+				sequence += number + " ";
+			}
+			if (mode.equals("v")){
+				System.out.print(sequence + "("+ count + ")");
+				System.out.println();
+			}
+
+		}
+			System.out.print("Every one of the first " + valueN + " hailstone sequences reached 1.");
 	}
 }
+
